d1a3bf80009476b00e5a5b70d261548ea9f4e99a
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-11-27 20:58:27 +0100

    SquadInfos

---
+++ src/s2clientprotocol.NET/SquadronTD/SquadInfos.cs
@@ -0,0 +1,10 @@
+﻿namespace s2clientprotocol.NET.SquadronTD;
+
+public record SquadInfos
+{
+    public bool BuildPhase { get; set; }
+    public uint Minerals { get; set; }
+    public uint Gas { get; set; }
+    public uint CurrentSupply { get; set; }
+    public uint MaxSupply { get; set; }
+}
