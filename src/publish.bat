06835b209f099b6330bc14b2e293b1785a0de5df
Firestorm-253 <laurin.zehnder@hotmail.com>
2024-09-29 17:13:40 +0200

    fix publish

--- src/publish.bat
+++ src/publish.bat
@@ -1,4 +1,7 @@
-dotnet build
+@echo off
+
+dotnet build --configuration Release
 dotnet pack
 set /p Version=Version: 
-dotnet nuget push "s2clientprotocol.NET\bin\Release\s2clientprotocol.NET.%Version%.nupkg"
+nuget push ".\s2clientprotocol.NET\bin\Release\s2clientprotocol.NET.%Version%.nupkg" -SkipDuplicate -NonInteractive
+pause
